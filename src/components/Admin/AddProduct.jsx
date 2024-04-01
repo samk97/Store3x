441b6f7bc8fafa393fbd40135128c359038008cd
amit-simplify3x <amit.k@simplify3x.com>
2024-04-01 16:11:28 +0530

    Admin pages

---
+++ src/components/Admin/AddProduct.jsx
@@ -0,0 +1,232 @@
+import React from "react";
+import Dashboard from "./Dashboard";
+
+const AddProduct = () => {
+  return (
+    <>
+      <Dashboard />
+      <section className="bg-white dark:bg-gray-900">
+        <div className="py-8 px-4 mx-auto max-w-2xl lg:py-16">
+          <div className="flex justify-center items-center">
+            <h2 className="mb-4 text-xl font-bold text-gray-900 dark:text-white">
+              Add a new product
+            </h2>
+          </div>
+          <form action="#">
+            <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="name"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Product Name
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  placeholder="Type product name"
+                  required=""
+                />
+              </div>
+              <div className="w-full">
+                <label
+                  htmlFor="brand"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Brand
+                </label>
+                <input
+                  type="text"
+                  name="brand"
+                  id="brand"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  placeholder="Product brand"
+                  required=""
+                />
+              </div>
+              <div className="w-full">
+                <label
+                  htmlFor="brand"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Price
+                </label>
+                <input
+                  type="text"
+                  name="brand"
+                  id="brand"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  placeholder="Product brand"
+                  required=""
+                />
+              </div>
+              <div className="w-full">
+                <label
+                  htmlFor="brand"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Rating
+                </label>
+                <input
+                  type="text"
+                  name="brand"
+                  id="brand"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  placeholder="Product brand"
+                  required=""
+                />
+              </div>
+              <div className="w-full">
+                <label
+                  htmlFor="brand"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Review Count
+                </label>
+                <input
+                  type="text"
+                  name="brand"
+                  id="brand"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  placeholder="Product brand"
+                  required=""
+                />
+              </div>
+              <div className="w-full">
+                <label
+                  htmlFor="brand"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Category Id
+                </label>
+                <input
+                  type="text"
+                  name="brand"
+                  id="brand"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  placeholder="Product brand"
+                  required=""
+                />
+              </div>
+              <div className="w-full">
+                <label
+                  htmlFor="brand"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Color
+                </label>
+                <input
+                  type="text"
+                  name="brand"
+                  id="brand"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  placeholder="Product brand"
+                  required=""
+                />
+              </div>
+              <div className="w-full">
+                <label
+                  htmlFor="brand"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  In stock
+                </label>
+                <input
+                  type="text"
+                  name="brand"
+                  id="brand"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  placeholder="Product brand"
+                  required=""
+                />
+              </div>
+              <div className="w-full">
+                <label
+                  htmlFor="price"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Image Url
+                </label>
+                <input
+                  type="text"
+                  name="price"
+                  id="price"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  placeholder="www.simlify3x.com"
+                  required=""
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="category"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Category
+                </label>
+                <select
+                  id="category"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                >
+                  <option selected="">Select category</option>
+                  <option value="TV">TV/Monitors</option>
+                  <option value="PC">PC</option>
+                  <option value="GA">Gaming/Console</option>
+                  <option value="PH">Phones</option>
+                </select>
+              </div>
+              <div>
+                <label
+                  htmlFor="item-weight"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Item Weight (kg)
+                </label>
+                <input
+                  type="number"
+                  name="item-weight"
+                  id="item-weight"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  placeholder={12}
+                  required=""
+                />
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="description"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Description
+                </label>
+                <textarea
+                  id="description"
+                  rows={8}
+                  className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  placeholder="Your description here"
+                  defaultValue={""}
+                />
+              </div>
+            </div>
+            <div className="flex justify-center items-center">
+              <button
+                type="submit"
+                className="inline-flex justify-center items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-red-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800"
+              >
+                Cancel
+              </button>
+              <button
+                type="submit"
+                className="inline-flex justify-center items-center px-5 py-2.5 ml-4 sm:mt-6 text-sm font-medium text-center text-white bg-red-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800"
+              >
+                Add Product
+              </button>
+            </div>
+          </form>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default AddProduct;
---
+++ src/components/Admin/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../UI/Header';
+const Dashboard = () => {
+  return (
+    <div>
+      <Header/>
+    </div>
+  )
+}
+
+export default Dashboard
